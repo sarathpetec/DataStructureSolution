80d8f674a69ed7aa4382c31d42e328035816efcd
Sarath <sarathjnair@gmail.com>
2020-05-31 01:03:32 +0200

    [] update test and maven error.

--- src/test/java/com/sarath/sample/demo/DemoApplicationTests.java
+++ src/test/java/com/sarath/sample/demo/DemoApplicationTests.java
@@ -1,16 +1,12 @@
 package com.sarath.sample.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void simpleTest() {
+		System.out.println("Simple Test");
 	}
 
 }
